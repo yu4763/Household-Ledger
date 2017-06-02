4ac54b305b32c105ce384f32d3ea4e1a6d0e5810
yu4763 <gkssk4763@naver.com>
2017-06-02 18:17:17 +0900

    제목이랑 버튼달기!! (프론트 페이지)

--- Budget/src/front/FrontPage.java
+++ Budget/src/front/FrontPage.java
@@ -7,9 +7,14 @@ import javax.swing.event.*;
 
 public class FrontPage extends JFrame{
 	
+	ImageIcon daram;
+	
 	FrontPage(){
+		
 		setTitle("다람이 가계부 프로그램");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		daram = new ImageIcon("./resources/darami.jpg");
 		
 		Container plate = getContentPane();
 		plate.setLayout(new BorderLayout(1700,1000));
@@ -21,16 +26,43 @@ public class FrontPage extends JFrame{
 		JLabel l = new JLabel(newIcon);
 		l.setSize(1700,1000);
 		l.setLocation(0,0);
-		plate.add(l);
 		
-		JButton button1 = new JButton("가계부 작성");
-		JButton button2 = new JButton("가계부 분석");
-	
-		plate.add(button1);
+		Font font1 = new Font("MD아트체", Font.BOLD, 100);
+		Font font2 = new Font("MD아트체", Font.PLAIN, 60);
+		
+		JLabel title = new JLabel("다람이 가계부");
+		title.setFont(font1);
+		title.setSize(800, 300);
+		title.setLocation(520, 180);
+		
+		JButton b1 = new JButton("가계부 작성");
+		JButton b2 = new JButton("가계부 분석");
+		
+		b1.setSize(400, 80);
+		b1.setLocation(280, 580);
+		b2.setSize(400, 80);
+		b2.setLocation(280, 680);
+		
+		b1.setBorderPainted(false);
+		b2.setBorderPainted(false);
+		b1.setBackground(new Color(255,233,157));
+		b2.setBackground(new Color(255,233,157));
+		b1.setFocusPainted(false);
+		b2.setFocusPainted(false);
+		
+		b1.setFont(font2);
+		b2.setFont(font2);
+		
+		l.add(title);
+		l.add(b1);
+		l.add(b2);
 		plate.add(l);
 		
 		setVisible(true);
 		setSize(1700,1000);
+		
+		
+		
 	}
 	
 	
